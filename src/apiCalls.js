81aea0ef1234e28fa31fd063eb41b3d40de3d5ab
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-07 09:35:51 -0700

    Add post fetch request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,13 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postRequest = (data) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  })
+}
